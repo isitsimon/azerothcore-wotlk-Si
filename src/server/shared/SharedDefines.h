9af09de2176931f57f63d637c3bf7c8aaeb21ed6
isitsimon <isitsimonyesitis@hotmail.co.uk>
2026-03-18 10:59:49 +0000

    Update race definitions in SharedDefines.h
    added in HighElf and Goblin
    
    moved Goblinold and Felorc down

--- src/server/shared/SharedDefines.h
+++ src/server/shared/SharedDefines.h
@@ -76,19 +76,21 @@ enum Races
     RACE_TAUREN             = 6,  // TITLE Tauren
     RACE_GNOME              = 7,  // TITLE Gnome
     RACE_TROLL              = 8,  // TITLE Troll
-    //RACE_GOBLIN             = 9,
+    //RACE_GOBLINOLD          = 23,
     RACE_BLOODELF           = 10, // TITLE Blood Elf
-    RACE_DRAENEI            = 11 //, TITLE Draenei
-    //RACE_FEL_ORC        = 12,
-    //RACE_NAGA           = 13,
-    //RACE_BROKEN         = 14,
-    //RACE_SKELETON       = 15,
-    //RACE_VRYKUL         = 16,
-    //RACE_TUSKARR        = 17,
-    //RACE_FOREST_TROLL   = 18,
-    //RACE_TAUNKA         = 19,
+    RACE_DRAENEI            = 11, // TITLE Draenei
+    //RACE_FEL_ORC            = 24,
+    //RACE_NAGA               = 13,
+    //RACE_BROKEN             = 14,
+    //RACE_SKELETON           = 15,
+    //RACE_VRYKUL             = 16,
+    //RACE_TUSKARR            = 17,
+    //RACE_FOREST_TROLL       = 18,
+    //RACE_TAUNKA             = 19,
     //RACE_NORTHREND_SKELETON = 20,
-    //RACE_ICE_TROLL      = 21
+    //RACE_ICE_TROLL          = 21,
+    RACE_GOBLIN             = 9,
+    RACE_HighElf             = 12
 };
 
 // DisplayRace values from CreatureDisplayInfoExtra.dbc
@@ -106,7 +108,7 @@ enum class DisplayRace : uint8
     Goblin            = 9,
     BloodElf          = 10,
     Draenei           = 11,
-    FelOrc            = 12,
+    HighElf           = 12,
     Naga              = 13,
     Broken            = 14,
     Skeleton          = 15,
@@ -115,7 +117,9 @@ enum class DisplayRace : uint8
     ForestTroll       = 18,
     Taunka            = 19,
     NorthrendSkeleton = 20,
-    IceTroll          = 21
+    IceTroll          = 21,
+    Goblinold         = 23,
+    Felord            = 24
 };
 
 // Class value is index in ChrClasses.dbc
